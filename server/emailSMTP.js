ebd9e0771f494c767f65a16d7daa5ea1635f4a5a
Igo Lapa <igo.lapa@tribalworldwide.co.uk>
2018-05-01 16:23:47 +0100

    Create server nodemailer

---
+++ server/emailSMTP.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const Email = require('email-templates');
+const router = express.Router();
+
+router.get('/test', (req, res, next) => {
+    res.send({ test: 'Yay, it worked'} );
+});
+
+router.post('/send', (req, res, next) => {
+
+    const nodeTransport = nodemailer.createTransport({
+        host: 'smtp.office365.com',
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: process.env.EMAIL_USER, // generated ethereal user
+            pass: process.env.EMAIL_PASS // generated ethereal password
+        },
+        tls: {
+            ciphers: 'SSLv3'
+        },
+        requireTLS: true
+    });
+
+    const email = new Email({
+        message: {
+            from: 'igo.lapa@tribalworldwide.co.uk'
+        },
+        send: true,
+        transport: nodeTransport
+    });
+    
+    email.send({
+        template: 'wfh',
+        message: {
+            to: 'igo.lapa@tribalworldwide.co.uk'
+        },
+        locals: {
+            name: 'Elon'
+        }
+    })
+    .then(function () {
+        res.status(200).end();
+    })
+    .catch(() => {
+        res.status(500).end();
+    });
+});
+
+module.exports = router;
